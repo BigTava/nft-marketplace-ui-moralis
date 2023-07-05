4a300f65764daedafd4fd162edf0dc18b0162ab6
Tiago Tavares <bigtavadev@gmail.com>
2023-07-05 17:16:02 +0100

    feat: handle operations

--- contracts/scripts/mint-and-list-item.ts
+++ contracts/scripts/mint-and-list-item.ts
@@ -6,7 +6,6 @@ const PRICE = ethers.utils.parseEther("0.1")
 async function mintAndList() {
     const nftMarketplace = await ethers.getContract("NftMarketplace")
     const randomNumber = Math.floor(Math.random() * 2)
-
     let basicNft
     if (randomNumber == 1) {
         basicNft = await ethers.getContract("BasicNftTwo")
@@ -17,15 +16,12 @@ async function mintAndList() {
     const mintTx = await basicNft.mintNft()
     const mintTxReceipt = await mintTx.wait(1)
     const tokenId = mintTxReceipt.events[0].args.tokenId
-
     console.log("Approving NFT...")
     const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
     await approvalTx.wait(1)
-
     console.log("Listing NFT...")
     const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
     await tx.wait(1)
-
     console.log("NFT Listed!")
     if (network.config.chainId == 31337) {
         // Moralis has a hard time if you move more than 1 at once!
